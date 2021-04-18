e4a7a404c770f711d3cd58d091334059f40dd8fb
angelo <aurtula1@gmail.com>
2021-04-18 03:46:56 -0400

    commit tree cd7300e3345e63b51f83f10c0b592399096fc195

--- spec/figures_controller_view_spec.rb
+++ spec/figures_controller_view_spec.rb
@@ -40,7 +40,7 @@ describe FiguresController do
   it "allows you to create a new figure with a landmark" do
     visit '/figures/new'
     fill_in :figure_name, :with => "Doctor Who"
-    check "landmark_#{Landmark.first.id}"
+    check("landmarks", allow_label_click: true)
     click_button "Create New Figure"
     figure = Figure.last
     expect(Figure.all.count).to eq(3)
